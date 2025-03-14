fbbf50aefefc11baef8f568a0bb82b2709a5d540
dimonodekolon <daluch@mail.ru>
2025-03-14 20:32:44 +0500

    task 2

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,63 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
 import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedList<E extends Serializable> extends AbstractList<E> {
 
+    private final File file;
+    private List<E> list;
+
+
     public SavedList(File file) {
+        this.file = file;
+        if (file.exists()) {
+            try (ObjectInputStream fis = new ObjectInputStream(new FileInputStream(file))) {
+                list = (ArrayList<E>) fis.readObject();
+            }catch (Exception ignored){}
+        }
+        else {
+            list = new ArrayList<>();
+        }
     }
 
     @Override
     public E get(int index) {
-        return null;
+        return list.get(index);
     }
 
     @Override
     public E set(int index, E element) {
-        return null;
+        E old = list.set(index, element);
+        this.write();
+        return old;
+
     }
 
     @Override
     public int size() {
-        return 0;
+        return list.size();
     }
 
     @Override
     public void add(int index, E element) {
+        list.add(index, element);
+        this.write();
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        E e = list.remove(index);
+        this.write();
+        return e;
+
+    }
+
+    private void write() {
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))){
+            oos.writeObject(list);
+        }catch (Exception ignored){}
     }
 }
